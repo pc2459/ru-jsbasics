c769d5175eec36bdd7267d7b3a92f7d70f592450
Po Linn Chia <chiapolinn@gmail.com>
2015-01-19 14:41:33 -0700

    Add comments, in need functionality

--- disaster.js
+++ disaster.js
@@ -1,10 +1,8 @@
-// var victimNames = [];
-// var victimNumbers = [];
-// var victimAddresses = [];
-
+// Store victims and volunteers in arrays
 var victims = [];
 var volunteers = [];
 
+// Use while loop to prompt users for victims
 var flag = true;
 var i = 0;
 while (flag){
@@ -18,6 +16,7 @@ while (flag){
   i++;
 }
 
+// Use while loop to prompt user for volunteers
 var flag2 = true;
 var j=0;
 while (flag2){
@@ -29,9 +28,9 @@ while (flag2){
 
   flag2 = confirm("Do you have another volunteer to add?");
   j++;
-
 }
 
+// Build victims and volunteers list to print to alert
 var victimsList = "";
 
 for (var i=0; i < victims.length; i++){
@@ -44,8 +43,33 @@ for (var i=0; i < volunteers.length; i++){
   volunteersList += volunteers[i].name + "\n";
 }
 
+// Alert the user
 alert("There are " + victims.length + " victims.\n" +
       "There are " + volunteers.length + " volunteers.\n" + 
       "The victims are: " + victimsList + 
       "The volunteers are: " + volunteersList);
 
+// Prompt user for name of person in need
+
+var personInNeed = prompt("Enter name of a person in need:", "John Snow");
+
+// Find the victim
+var found = 0;
+for(i=0;i<victims.length;i++){
+  if(victims[i].name === personInNeed){
+    found = i;
+  }
+}
+
+// Build a list of volunteers on the same street
+var sameStreetList = "";
+
+for(j=0;j<volunteers.length;j++){
+  if(victims[found].street === volunteers[j].street){
+    sameStreetList += volunteers[j].name + "\n";
+  }
+}
+
+// Alert the user
+alert("The following volunteers are on the same street as " + personInNeed + ":\n" 
+        + sameStreetList);
